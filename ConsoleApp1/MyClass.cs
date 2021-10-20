00493aab86194222e84cea4a4a2ca3f11feed3be
SwiftKillers <alieksiei.gholubiev.2003@mail.ru>
2021-10-20 09:44:14 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/MyClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class MyClass
+    {
+        string x;
+        public string X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public string this[string index]
+        {
+            get { return (index == "0") ? x : null; }
+            set { if (index == "0") x = value; }
+        }
+
+        public string this[int index]
+        {
+            get { return new string[] { x }[index]; }
+            set { x = value; }
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+            myClass.X = "3";
+            string x = myClass.X;
+            myClass[0] = "4";
+            x = myClass[0];
+        }
+    }
+}
